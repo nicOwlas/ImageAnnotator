a2d4171a8f0de8b4680581d42ed79dea514ad1db
Nicolas Draber <nicolas.draber@gmail.com>
2024-06-17 21:31:03 +0200

    Possible to draw lines on image

--- app/components/Canvas.tsx
+++ app/components/Canvas.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "ol/ol.css";
 import { Map } from "@react-ol/fiber";
+import { Stroke, Fill } from "ol/style";
 import { getCenter } from "ol/extent";
 import Projection from "ol/proj/Projection";
 
@@ -11,6 +12,17 @@ export const projection = new Projection({
   extent,
 });
 
+export const fill = new Fill({
+  color: "rgba(255, 255, 255, 0.2)",
+});
+export const stroke = new Stroke({
+  color: "#ffcc33",
+  width: 2,
+});
+export const circleFill = new Fill({
+  color: "#ffcc33",
+});
+
 const Canvas = ({
   imagePath,
   imageExtent,
@@ -18,6 +30,7 @@ const Canvas = ({
   imagePath: string;
   imageExtent: [number, number, number, number];
 }) => {
+  const [vectorSource, setVectorSource] = useState();
   return (
     <Map style={{ width: "100%", height: "100%", position: "fixed" }}>
       <olView
@@ -34,6 +47,31 @@ const Canvas = ({
           imageExtent={imageExtent}
         />
       </olLayerImage>
+      <olLayerVector>
+        <olSourceVector ref={setVectorSource} />
+        <olStyleStyle attach="style" fill={fill} stroke={stroke}>
+          <olStyleCircle
+            attach="image"
+            args={{ radius: 7, fill: circleFill }}
+          />
+        </olStyleStyle>
+      </olLayerVector>
+      {vectorSource ? (
+        <>
+          <olInteractionModify source={vectorSource} />
+          <olInteractionDraw
+            args={{
+              type: "LineString",
+              source: vectorSource,
+              snapTolerance: 30,
+            }}
+          />
+          <olInteractionSnap
+            source={vectorSource}
+            args={{ pixelTolerance: 30 }}
+          />
+        </>
+      ) : null}
     </Map>
   );
 };
